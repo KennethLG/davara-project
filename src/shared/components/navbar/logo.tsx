2cf37f54482e229f81ec7c464c64d133b0b30f18
KennethLG <orsonleiziaga@gmail.com>
2022-11-19 20:56:04 -0500

    Navbar component

--- src/shared/components/navbar/logo.tsx
+++ src/shared/components/navbar/logo.tsx
@@ -1,5 +1,5 @@
 export const Logo = () => {
   return (
-    <img className="h-10 rounded-full" src="https://picsum.photos/200/200" />
+    <img className='h-10 rounded-full' src='https://picsum.photos/200/200' />
   );
 };
--- src/shared/components/navbar/navbar.tsx
+++ src/shared/components/navbar/navbar.tsx
@@ -1,10 +1,46 @@
 import { Logo } from './logo';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { useState } from 'react';
 
 export const Navbar = () => {
+  const [showNavbarMenu, setShowNavbarMenu] = useState(false);
+
+  const links = [
+    {
+      title: 'Blog',
+      link: '/blog',
+    },
+    {
+      title: 'Casos de estudio',
+      link: '/casos-de-estudio',
+    },
+    {
+      title: 'Consultas',
+      link: '/consultas',
+    },
+    {
+      title: 'Hablemos',
+      link: '/conferencias',
+    },
+  ];
+
   return (
-    <div className='fixed top-0 left-0 p-3 my-auto w-full border border-slate-300 flex items-center gap-x-2'>
-      <Logo />
-      <h2>this is a navbarrr</h2>
+    <div className='fixed top-0 left-0 p-3 my-auto w-full border border-slate-300'>
+      <div className='flex flex-row justify-around items-center gap-x-2'>
+        <Logo />
+        <h2>Davara</h2>
+
+        <GiHamburgerMenu onClick={() => setShowNavbarMenu(!showNavbarMenu)} />
+      </div>
+      {showNavbarMenu && (
+        <section className='flex flex-col items-start my-2'>
+          {links.map((link, i) => (
+            <article className='px-8 py-1' key={i}>
+              {link.title}
+            </article>
+          ))}
+        </section>
+      )}
     </div>
   );
 };
