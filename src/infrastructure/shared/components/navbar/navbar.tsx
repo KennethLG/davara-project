6c5578170317c509991e29095aa64c02e67dd7dc
KennethLG <orsonleiziaga@gmail.com>
2022-11-21 15:57:27 -0500

    Separate navbar component

--- src/infrastructure/shared/components/navbar/navbar.tsx
+++ src/infrastructure/shared/components/navbar/navbar.tsx
@@ -2,46 +2,27 @@ import { Logo } from './logo';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { useState } from 'react';
 import { CustomLink } from '../customLink/customLink';
+import { NavList } from './list';
 
 export const Navbar = () => {
   const [showNavbarMenu, setShowNavbarMenu] = useState(false);
 
-  const routes = [
-    {
-      title: 'Blog',
-      link: '/blog',
-    },
-    {
-      title: 'Casos de estudio',
-      link: '/casos-de-estudio',
-    },
-    {
-      title: 'Consultas',
-      link: '/consultas',
-    },
-    {
-      title: 'Hablemos',
-      link: '/conferencias',
-    },
-  ];
+  const switchNavbarMenu = () => () => setShowNavbarMenu(!showNavbarMenu);
 
   return (
-    <div className='fixed top-0 left-0 p-3 my-auto w-full border border-slate-300'>
+    <nav className='fixed top-0 left-0 p-3 my-auto w-full border border-slate-300'>
       <div className='flex flex-row justify-around items-center gap-x-2'>
-        <Logo />
+        <CustomLink to='/'>
+          <Logo />
+        </CustomLink>
         <h2>Davara</h2>
 
-        <GiHamburgerMenu onClick={() => setShowNavbarMenu(!showNavbarMenu)} />
+        <GiHamburgerMenu
+          className='cursor-pointer'
+          onClick={switchNavbarMenu()}
+        />
       </div>
-      {showNavbarMenu && (
-        <section className='flex flex-col items-start my-2'>
-          {routes.map((route, i) => (
-            <CustomLink key={i} to={route.link}>
-              <article className='px-8 py-1'>{route.title}</article>
-            </CustomLink>
-          ))}
-        </section>
-      )}
-    </div>
+      <NavList display={showNavbarMenu} handleClick={switchNavbarMenu()} />
+    </nav>
   );
 };
